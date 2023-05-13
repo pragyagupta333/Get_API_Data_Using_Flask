9f2f0eb29ea3ecbd1ad0b8cf5d0142c809d06325
pragyagupta333 <125549428+pragyagupta333@users.noreply.github.com>
2023-05-13 22:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import json
-import jinja2
+import jinja2 
 import requests # To sends HTTP requests => GET,POST 
 from flask import render_template
 from jinja2 import TemplateNotFound
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/',methods=['GET', 'POST']) # .route tells @app That whenever user visits app domain execute index() function
 def index():
-   req = requests.get('https://jsonplaceholder.typicode.com/todos/')
-   reqdata = req.json()
+   req = requests.get('https://jsonplaceholder.typicode.com/todos/') # Fetch Data From API Into Req
+   reqdata = req.json() # Converts Data To JSON Format
    # for i in range(len(reqdata)):
    #  print(reqdata)
-   return render_template('index.html', newdata=reqdata)
+   return render_template('index.html', newdata=reqdata) # Passes API Data as "newdata" Into File index.html
 
 
 if __name__ == '__main__':
